f2a19d5e5eac86b2e7f1f4bf5f6437b5a3c48950
samfergar <161924858+samfergar@users.noreply.github.com>
2024-03-02 11:20:43 +0100

    Update rectangulos.py

--- rectangulos.py
+++ rectangulos.py
@@ -31,17 +31,19 @@ def putRectangle(frame, centre_x, centre_y, length, width, ):
     
     
 def putScope(frame, centre_x, centre_y, size, scope_size):
-    centre_topleft = (centre_x - size, centre_y + 2*size)
-    centre_bottomright = (centre_x + size, centre_y - 2*size)
+    centre_topleft = (centre_x - size, centre_y + size)
+    centre_bottomright = (centre_x + size, centre_y - size)
     
     #top point
-    cv2.rectangle(frame, (centre_topleft[0],centre_topleft[1] + scope_size), (centre_bottomright[0], centre_bottomright[1] + scope_size), (0,0,0), -1)
+    cv2.rectangle(frame, (centre_topleft[0],centre_topleft[1] + 100*scope_size), (centre_bottomright[0], centre_bottomright[1] + scope_size), (0,255,0), -1)
      
     #right point
-    cv2.rectangle(frame, (centre_topleft[0] + scope_size,centre_topleft[1]) , (centre_bottomright[0]+scope_size, centre_bottomright[1]), (0,0,0), -1)
+    cv2.rectangle(frame, (centre_topleft[0] + scope_size,centre_topleft[1]) , (centre_bottomright[0]+100*scope_size, centre_bottomright[1]), (0,255,0), -1)
      
     #left point
-    cv2.rectangle(frame, (centre_topleft[0] -  scope_size,centre_topleft[1]) , (centre_bottomright[0]-scope_size, centre_bottomright[1]) ,(0,0,0), -1)
+    cv2.rectangle(frame, (centre_topleft[0] -  scope_size,centre_topleft[1]) , (centre_bottomright[0]-100*scope_size, centre_bottomright[1]) ,(0,255,0), -1)
     
     #down point
-    cv2.rectangle(frame, (centre_topleft[0], centre_topleft[1]-scope_size) , (centre_bottomright[0] , centre_bottomright[1] - scope_size), (0,0,0), -1)
+    cv2.rectangle(frame, (centre_topleft[0], centre_topleft[1]-100*scope_size) , (centre_bottomright[0] , centre_bottomright[1] - scope_size), (0,255,0), -1)
+
+cap = cv2.VideoCapture(0)
